2a38333240132dc920e7b82986578660226fab7e
Abigail <abigailtcameron@gmail.com>
2024-12-29 22:05:42 -0600

    search update

--- src/components/explore/trending/TrendingBooks.tsx
+++ src/components/explore/trending/TrendingBooks.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { usePathname, useRouter } from 'next/navigation';
-import { Book } from '../../../../..';
+import React from 'react'
+import { useRouter } from 'next/navigation';
 import StarRating from '../../StarRating';
 import TrendBookImage from './TrendBookImage';
+import { Book } from '../../../..';
 
 type Props = {
   books: Book[]; 
