34abb76d540d6685a7453263f2d334b31d6cb3e4
Abigail <abigailtcameron@gmail.com>
2025-01-17 16:56:21 -0600

    new social

---
+++ src/components/icons/X.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+type Props = {
+  className?: string;
+};
+
+function X({ className }: Props) {
+  return (
+    <svg
+      width="40px"
+      height="40px"
+      viewBox="0 0 24 24"
+      strokeWidth="1.4"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      color="#000000"
+      className={className}
+    >
+      <path
+        d="M16.8198 20.7684L3.75317 3.96836C3.44664 3.57425 3.72749 3 4.22678 3H6.70655C6.8917 3 7.06649 3.08548 7.18016 3.23164L20.2468 20.0316C20.5534 20.4258 20.2725 21 19.7732 21H17.2935C17.1083 21 16.9335 20.9145 16.8198 20.7684Z"
+        strokeWidth="1.4"
+      ></path>
+      <path
+        d="M20 3L4 21"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+      ></path>
+    </svg>
+  );
+}
+
+export default X;
