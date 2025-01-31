4d7e8c58ffc88c3629c26438b88ad8f72eddec63
Abigail <abigailtcameron@gmail.com>
2025-01-31 01:20:34 -0600

    add price and click into

--- src/components/profile/ProfileBooks.tsx
+++ src/components/profile/ProfileBooks.tsx
@@ -4,6 +4,7 @@ import { Book } from '../../..';
 import Clock from '../icons/Clock';
 import { timeAgo } from '../../../tools/timeago';
 import { formatDate } from '../../../tools/formatDate';
+import View from '../icons/View';
 
 type Props = {
   books?: string[];
@@ -34,16 +35,27 @@ function ProfileBooks({books}: Props) {
                     alt={book.title}
                   />
 
-                  <div className="absolute bottom-0 bg-[#1b1c1e] w-full h-fit rounded-b-3xl">
+                  <div className="flex flex-col absolute bottom-0 bg-[#1b1c1e] w-full h-1/3 rounded-b-3xl px-4 py-4 space-y-2">
+
+                    <p className="text-lg font-semibold">{book.title}</p>
 
                     <div>
                       <p className="text-sm">Price</p>
                     </div>
-                    <div className="flex text-sm items-center">
-                        <Clock 
-                          className="stroke-white size-5"
+
+                    <div className="flex justify-between">
+                      <div className="flex text-xs space-x-1 items-center">
+                          <Clock 
+                            className="stroke-white size-3"
+                          />
+                          <p>{formatDate(book.created_at)}</p>
+                      </div>
+
+                      <div className="flex text-xs">
+                        <View 
+                          className="stroke-white size-4"
                         />
-                        <p>{formatDate(book.created_at)}</p>
+                      </div>
                     </div>
                    
                   </div>
