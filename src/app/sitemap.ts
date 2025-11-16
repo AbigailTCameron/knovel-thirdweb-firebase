32270b0668e1e4cd067021ded81203155ff5f5ce
Abigail <abigailtcameron@gmail.com>
2025-11-16 12:41:07 -0600

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,62 @@
+import type { MetadataRoute } from 'next';
+import initializeFirebaseServer from "@/lib/initFirebaseAdmin";
+
+
+const { db } = initializeFirebaseServer();
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://knovelprotocol.com"; 
+  const now = new Date();
+
+  const staticRoutes: MetadataRoute.Sitemap = [
+    {
+      url: `${baseUrl}/`,
+      lastModified: now,
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/explore`,
+      lastModified: now,
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/search`,
+      lastModified: now,
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/collection`,
+      lastModified: now,
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+  ];
+
+  const booksSnap = await db.collection("books").get();
+  const bookRoutes: MetadataRoute.Sitemap = booksSnap.docs.map((doc) => {
+      const data = doc.data() as any;
+
+    const updated =
+      (data.updated_at && data.updated_at.toDate?.()) ||
+      (data.created_at && data.created_at.toDate?.()) ||
+      now;
+
+    const bookUrl = `${baseUrl}/book/${doc.id}`;
+    const imageUrl = data.book_image as string | "";
+
+    return {
+      url: bookUrl,
+      lastModified: updated,
+      changeFrequency: "weekly",
+      priority: 0.7,
+      images: [imageUrl]
+    }
+
+  })
+
+
+  return [...staticRoutes, ...bookRoutes]
+}
